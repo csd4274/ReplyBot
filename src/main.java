85f3642381e5c541f763712f290843bba7e69b86
csd4274 <csd4274@csd.uoc.gr>
2025-05-25 10:28:02 +0300

    Deleted old files from repo

--- src/main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-        READFILE rf = READFILE.getInstance();
-        bot robot = bot.getInstance();
-        System.out.println(robot.Reply("ABoUT YOURSELF"));
-    }
-}
